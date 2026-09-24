e0a1aa1936ccea47c2473dffc60f7f8c366e613e
Fabian <fabianjoseph063@gmail.com>
2026-09-24 21:47:15 +0100

    fix: remove unused import of Type from MainPage component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight, Type } from 'lucide-react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Link } from 'react-router'
 import { ScrollToSection } from '../ui/ScrollAssist';
 import { CodeBlock } from '../ui/CodeBlock/CodeBlock';
